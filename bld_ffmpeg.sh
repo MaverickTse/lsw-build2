270b67340a49ce00b58b8cc0f9b0c83aeaf8f82b
MaverickTse@gmail.com <MaverickTse@gmail.com>
2015-03-05 15:58:52 +0800

    set target-os and disable-w32threads

--- bld_ffmpeg.sh
+++ bld_ffmpeg.sh
@@ -8,12 +8,12 @@ if [ ! -d .git ]; then
     git clone -v --progress --config core.autocrlf=false https://github.com/FFmpeg/FFmpeg.git ./
 fi
 git pull -v --progress
-./configure --prefix="/mingw32" \
-  --extra-cflags="-fexcess-precision=fast -I/mingw/include/openjpeg-1.5" \
+./configure --prefix="/mingw32" --target-os=mingw32 \
+  --extra-cflags="-fexcess-precision=fast -I/mingw32/include/openjpeg-1.5" \
   --enable-static --enable-avresample --enable-memalign-hack \
   --enable-pthreads --enable-runtime-cpudetect --enable-gpl \
   --enable-version3 --enable-libopus --enable-libopenjpeg \
   --enable-avisynth --disable-doc --disable-debug \
-  --disable-network --disable-shared
+  --disable-network --disable-shared --disable-w32threads
 make clean
 make -j$(nproc) && make install
