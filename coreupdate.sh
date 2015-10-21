2f04ae93f494fb4816a34dbe9014d237632c1cd6
MaverickTse <mavericktse@yahoo.co.jp>
2015-10-21 15:56:19 +0800

    use the included update-core command

--- coreupdate.sh
+++ coreupdate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-pacman -Sy
-pacman --needed --noconfirm -S bash pacman pacman-mirrors msys2-runtime
+#pacman -Sy
+#pacman --needed --noconfirm -S bash pacman pacman-mirrors msys2-runtime
+update-core
