a7b67d5acf6ba5c964c4f1df3767395b0752f7b8
AinoMegumi <aimegu2014@gmail.com>
2015-10-28 23:12:21 +0900

    check for $MSYSTEM at the start of every build script  #13

--- bld_ls_64.sh
+++ bld_ls_64.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $MSYSTEM != "MINGW64" ]; then
+echo "You MUST launch MSYS2 using mingw64_shell.bat"
+echo "OR set the PROCESS environment variable: MSYSTEM , to 'MINGW64', prior launching mintty.exe"
+exit
+else
 if [ ! -d l-smash_64 ]; then
     mkdir l-smash_64
 fi
--- bld_zlib.sh
+++ bld_zlib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $MSYSTEM != "MINGW32" ]; then
+echo "You MUST launch MSYS2 using mingw32_shell.bat"
+echo "OR set the PROCESS environment variable: MSYSTEM , to 'MINGW32', prior launching mintty.exe"
+exit
+else
 if [ ! -d zlib ]; then
     mkdir zlib
 fi
